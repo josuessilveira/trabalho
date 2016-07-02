ebc530d357ccb97dd9d9c4fa825fcc39f0fadb7d
josuessilveira <josuessilveira@yahoo.com.br>
2016-07-01 21:58:16 -0300

    novo commit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Nova Tela alterada</title>
+        <title>Nova qualquer coisa</title>
     </head>
     <body>
         <?php
