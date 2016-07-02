77e8d785fe0d110523763aa245551c01c9662b29
josuessilveira <josuessilveira@yahoo.com.br>
2016-07-01 22:02:29 -0300

    novo commit yuri

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Nova qualquer coisa</title>
+        <title>Nova yuri</title>
     </head>
     <body>
         <?php
