a28bafc2aa4bc5495297eececcfa797199d306a9
Yuri Jacques <yurinarvaisjacques@gmail.com>
2016-07-08 16:28:26 -0300

    Testar Interação Git e Hostinger

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Nova Tela</title>
+        <title>Teste Git e Hostinger</title>
     </head>
     <body>
         <?php
