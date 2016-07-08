82a0655b16dd4482fca313527297bbc949b70ef5
josuessilveira <josuessilveira@yahoo.com.br>
2016-07-08 14:11:44 -0300

    novo commitjosue

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Nova yuri</title>
+        <title>Nova josue</title>
     </head>
     <body>
         <?php
